4b8e9175c40c357fa30cb73b5ebbbbbabb5b58dd
himmeldev <himmeldev@gmail.com>
2021-10-03 22:12:23 -0300

    Created the `setCD` function.

--- src/Client/Functions.ts
+++ src/Client/Functions.ts
@@ -1,5 +1,6 @@
-import { Message, MessageOptions } from "discord.js";
-import { D } from "../Util/Interfaces";
+import { Guild, Message, MessageOptions, User } from "discord.js";
+import ms from "ms";
+import { Command, D } from "../Util/Interfaces";
 import { contactFunction } from "../Util/ContactDeveloper";
 import { Bot } from "./Bot";
 
@@ -14,6 +15,49 @@ export const Functions = {
 		};
 
 		return await message.channel.send(d).catch(async (err) => await message.channel.send({ ...data }).catch(async (err) => await contactFunction(client, err)));
+	},
+	setCD: async (command: Command, target: string, d: D) => {
+		const { cooldown } = command;
+		const { client } = d;
+		const { cds } = client;
+
+		if (cooldown.time === "none") return true;
+
+		const time = ms(cooldown.time);
+		if (!cds.has(command.name)) cds.set(command.name, {});
+
+		let t: User | Guild;
+
+		switch (cooldown.type) {
+			case "user":
+				t = client.users.cache.get(target) || (await client.users.fetch(target).catch((err) => null));
+				break;
+			case "guild":
+				t = client.guilds.cache.get(target);
+				break;
+		}
+
+		if (!t) return true;
+
+		const now = Date.now();
+		const expiration = Math.floor((cds.get(command.name)[target] || 0) + Number(time));
+		const all = cds.get(command.name);
+
+		if (now < expiration && !command.testing) return false;
+
+		if (now > expiration) {
+			all[target] = 0;
+
+			cds.set(command.name, all);
+
+			return true;
+		}
+
+		all[target] = now + Number(time);
+
+		cds.set(command.name, all);
+
+		return false;
 	}
 };
 
