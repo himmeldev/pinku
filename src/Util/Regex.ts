626c21e8ca323ccb3dacf200b123c4604fca99ad
himmeldev <himmeldev@gmail.com>
2021-10-08 09:31:12 -0300

    Added `urlRegex` and `imgRegex` in `Regex.ts`

--- src/Util/Regex.ts
+++ src/Util/Regex.ts
@@ -1,1 +1,14 @@
+/**
+ * @returns string[] - Array of ID-Like numbers.
+ */
 export const idRegex = /[0-9]{10,30}/g;
+
+/**
+ * @returns string[] - Array of urls.
+ */
+export const urlRegex = /https{0,1}:\/\/[\w]+\.[a-z]+\/{0,1}[\w]*/gi;
+
+/**
+ * @returns string[] - Array of image urls.
+ */
+export const imgRegex = /https{0,1}:\/\/[\w]+\.[a-z]+\/(?:[\w/]+)?[\w]+\.(png|jpg|jpeg|gif)/g;
