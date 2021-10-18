e6cd6cd9a6142bef2ad66f7cf65b4800e96cd365
Himmel <83486006+himmeldev@users.noreply.github.com>
2021-10-18 13:10:25 -0300

    now regex functions returns [] instead of null

--- src/Util/Regex.ts
+++ src/Util/Regex.ts
@@ -3,5 +3,5 @@ export const mentionRegex = /<@(!|#|&){0,1}([0-9]{10,30})>/g;
 export const urlRegex = /https{0,1}:\/\/[\w]+\.[a-z]+\/{0,1}[\w]*/gi;
 export const imgRegex = /https{0,1}:\/\/[\w]+\.[a-z]+\/(?:[\w/]+)?[\w]+\.(png|jpg|jpeg|gif)/g;
 
-export const findURLs = (text: string, type: "img" | "url") => text.match(type === "img" ? imgRegex : urlRegex);
-export const findMentions = (text: string, type: "ids" | "full") => text.match(type === "ids" ? idRegex : mentionRegex);
+export const findURLs = (text: string, type: "img" | "url") => text.match(type === "img" ? imgRegex : urlRegex) || [];
+export const findMentions = (text: string, type: "ids" | "full") => text.match(type === "ids" ? idRegex : mentionRegex) || [];
