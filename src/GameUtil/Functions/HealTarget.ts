83cc8d879f8bfc439a27c0a005894b53d645d4e8
himmeldev <himmeldev@gmail.com>
2021-10-19 02:01:51 -0300

    Creation of `HealTarget` function.

---
+++ src/GameUtil/Functions/HealTarget.ts
@@ -0,0 +1,35 @@
+import { D } from "../../Util/Interfaces";
+import { HealingItem, Player } from "../Interfaces";
+
+export const HealTarget = (d: D, player: Player, item: HealingItem): Player => {
+	let { Buffs, Debuffs, HealAmount, Cooldown, HealType } = item;
+
+	if (Buffs[0] || Debuffs[0]) {
+		// TODO: Create process to apply buffs and debuffs to the Player.
+	}
+
+	switch (HealType) {
+		case "life":
+			player.hp.current += HealAmount;
+			break;
+		case "special":
+			const r = HealAmount - (player.hp.special.max - player.hp.special.value);
+
+			if (r < 0) {
+				player.hp.special.value += HealAmount;
+                if (player.hp.special.value > player.hp.special.max) player.hp.special.value = player.hp.special.max;
+				break;
+			}
+
+			player.hp.special.value = player.hp.special.max;
+			player.hp.current += r;
+
+			break;
+	}
+
+    // TODO: Cooldown implementation.
+
+	if (player.hp.current > player.hp.max) player.hp.current = player.hp.max;
+
+	return player;
+};
