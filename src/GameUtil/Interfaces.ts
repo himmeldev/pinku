0163c15f60c6bfd697dcaf4025a9d71206d0ee0e
himmeldev <himmeldev@gmail.com>
2021-10-19 01:51:23 -0300

    I fucked up how the values of 'special' were made

--- src/GameUtil/Interfaces.ts
+++ src/GameUtil/Interfaces.ts
@@ -5,42 +5,45 @@ export interface Player {
 	hp: {
 		max: number;
 		current: number;
-        special: number;
+		special: {
+			value: number;
+			max: number;
+		};
 	};
 }
 
 export interface HealingItem {
 	HealAmount: number;
-    /**
-     * HealAmount property can be negative, this will allow the possibility of items like:
-     * "Consumes 20 life and gives buff <SomeRandomBuff> which gives +20 defense."
-     */
+	/**
+	 * HealAmount property can be negative, this will allow the possibility of items like:
+	 * "Consumes 20 life and gives buff <SomeRandomBuff> which gives +20 defense."
+	 */
 	HealType: "life" | "special";
 	/**
 	 * Type "life": Default current/MaxLife.
 	 * Type "special": SpecialBuff + Default current/MaxLife.
 	 */
 	Cooldown: string;
-    /**
-     * Cooldown format example: "12s"
-     */
-    Buffs?: Effect[];
-    Debuffs?: Effect[];
+	/**
+	 * Cooldown format example: "12s"
+	 */
+	Buffs?: Effect[];
+	Debuffs?: Effect[];
 }
 
 export interface Effect {
-    Type: "buff" | "debuff";
-    EffectType: "player" | "party";
-    Description: string;
-    Stats: Stat[];
+	Type: "buff" | "debuff";
+	EffectType: "player" | "party";
+	Description: string;
+	Stats: Stat[];
 }
 
 export interface Stat {
-    name: "defense" | "life" | "TestingGodMode";
-    value: number | "max";
-    /**
-     * Example value: +5 | -20 | 1
-     * Value will be used to 'set' the value within the player data.
-     * Practical Explanation in "Stats Explanation."
-     */
+	name: "defense" | "life" | "TestingGodMode";
+	value: number | "max";
+	/**
+	 * Example value: +5 | -20 | 1
+	 * Value will be used to 'set' the value within the player data.
+	 * Practical Explanation in "Stats Explanation."
+	 */
 }
