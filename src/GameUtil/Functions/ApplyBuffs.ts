69c63c96154399adb05a2149002e23a6e41f2353
himmeldev <himmeldev@gmail.com>
2021-11-01 11:46:50 -0300

    Minimal cleanup

--- src/GameUtil/Functions/ApplyBuffs.ts
+++ src/GameUtil/Functions/ApplyBuffs.ts
@@ -1,4 +1,3 @@
-import { D } from "../../Util/Interfaces";
 import { Effect, Player } from "../Interfaces";
 import { ApplyStats } from "./ApplyStats";
 
@@ -6,7 +5,7 @@ export const ApplyBuffs = (Target: Player, BuffList: Effect[]): Player => {
 	for (let i = 0; i < BuffList.length; i++) {
 		const CurrentEffect: Effect = BuffList[i];
 
-		const _i = Target[CurrentEffect.Type + "s"].findIndex((f: Effect, i: number) => f.Name === CurrentEffect.Name);
+		const _i = Target[CurrentEffect.Type + "s"].findIndex((f: Effect) => f.Name === CurrentEffect.Name);
 		if (_i < 0) {
 			Target[CurrentEffect.Type + "s"].push(CurrentEffect);
 			continue;
