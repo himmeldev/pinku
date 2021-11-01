dbe26bfd727c7aeb39bc748bd5f06acd9a4b7bb5
himmeldev <himmeldev@gmail.com>
2021-11-01 11:15:57 -0300

    Removed unused definitions

--- src/GameUtil/Functions/ApplyStats.ts
+++ src/GameUtil/Functions/ApplyStats.ts
@@ -1,22 +1,11 @@
-import { D } from "../../Util/Interfaces";
 import { Player, Stat } from "../Interfaces";
-import { lifeStat } from "../Stats";
+import * as AllStats from "../Stats";
 
-export const ApplyStats = (d: D, Target: Player, Stats: Stat[]): Player => {
-	Stats.map((stat: Stat, i: number) => {
-		const st = findValue(stat);
+export const ApplyStats = (Target: Player, Stats: Stat[]): Player => {
+	Stats.map((stat: Stat) => {
+		const st = AllStats[stat.name];
 		Target.stats[stat.name] = stat.value === "max" ? st.MaxValue : stat.value > st.MaxValue ? st.MaxValue : stat.type === "upgrade" ? (Target.stats[stat.name] + stat.value > st.MaxValue ? st.MaxValue : Target.stats[stat.name] + stat.value) : stat.value;
 	});
 
-    return Target
-};
-
-const findValue = (stat: Stat) => {
-	switch (stat.name) {
-		case "life":
-			return lifeStat;
-
-		default:
-			return { MinimumValue: 0, MaxValue: 0, DefaultValue: 0 };
-	}
+	return Target;
 };
